89c6f4ffb94a50a02f8694eaeadb8614361695ce
WirWollen <52575418+WirWollen@users.noreply.github.com>
2023-10-15 02:06:31 +0400

    refactoring

--- src/main/java/vtb/map/map/repo/WorkloadRepo.java
+++ src/main/java/vtb/map/map/repo/WorkloadRepo.java
@@ -9,6 +9,8 @@ import java.util.List;
 public interface WorkloadRepo extends CrudRepository<WorkloadEntity, Long> {
     List<WorkloadEntity> findAll();
 
-    @Query(value = "SELECT COUNT(*) FROM workload WHERE time > ?1 AND time < ?2 AND active = true", nativeQuery = true)
-    Integer calculateClients(LocalDateTime startTime, LocalDateTime endTime);
+    @Query(value = "SELECT COUNT(*) FROM workload " +
+            "JOIN department ON workload.department_entity_id = department.id " +
+            "WHERE time > ?1 AND time < ?2 AND active = true AND locality_entity_id = ?3", nativeQuery = true)
+    Integer calculateClients(LocalDateTime startTime, LocalDateTime endTime, Long localityId);
 }
